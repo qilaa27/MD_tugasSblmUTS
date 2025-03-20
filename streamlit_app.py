de319f53a54895dc3f65c56040214e6622fb412f
qilaa27 <140408048+qilaa27@users.noreply.github.com>
2025-03-20 22:09:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 import joblib
 
+def load_model(filename):
+  model = joblib.load(filename)
+  return model
+
+def predict_with_model(model, user_input):
+  prediction = model.predict([user_input])
+  return prediction[0]
+
+def main():
+  st.title('Machine Learning App')
+  st.info('TThis app will predict your abesity level!')
